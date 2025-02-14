0c2e27d518071e622c109bae61c88ac5e85f6e83
suryaa4419@gmail.com <surya4419@gmail.com>
2025-02-14 14:47:15 +0530

    frontend completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import AddCourse from './pages/educator/AddCourse';
 import MyCourses from './pages/educator/MyCourses';
 import StudentsEnrolled from './pages/educator/StudentsEnrolled';
 import Navbar from './components/student/Navbar';
+import "quill/dist/quill.snow.css"
 
 const App = () => {
 
--- client/src/pages/educator/Dashboard.jsx
+++ client/src/pages/educator/Dashboard.jsx
@@ -43,7 +43,35 @@ const Dashboard = () => {
           </div>
         </div>
 
-        
+         <div>
+          <h2 className='pb-4 text-lg font-medium'>Latest Enrollments</h2>
+          <div className='flex flex-col items-center max-w-4xl w-full overflow-hidden rounded-md bg-white border border-gray-500/20'>
+            <table className='table-fixed md:table-auto w-full overflow-hidden'>
+              <thead className='text-gray-900 border-b border-gray-500/20 text-sm text-left'>
+                <tr>
+                  <th className='px-4 py-3 font-semibold text-center hidden sm:table-cell'>
+                    #
+                  </th>
+                  <th className='px-4 py-3 font-semibold'>Student Name</th>
+                  <th className='px-4 py-3 font-semibold'>Course Title</th>
+                </tr>
+              </thead>
+              <tbody className='text-sm text-gray-500'>
+                {dashboardData.enrolledStudentsData.map((item,index) =>(
+                  <tr key={index} className='border-b border-gray-500/20'>
+                    <td className='px-4 py-3 text-center hidden sm:table-cell'>{index +1}</td>
+                    <td className='md:px-4 px-2 flex items-center space-x-3 '>
+                      <img src={item.student.imageUrl} alt="profile" className='w-9 h-9 rounded-full' />
+                      <span className='truncate'>{item.student.name}</span>
+                    </td>
+                    <td className='px-4 py-3 truncate'>{item.courseTitle}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+         </div>
+         
       </div>
     </div>
   ) : <Loading/>
